b7a01022f31c7c8211c1000703d8cb7f56d8b201
Sam.Z.D <samerzmd@gmail.com>
2021-11-01 16:44:04 +0200

    1: Best Practices For Application Deployment

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,13 +1,19 @@
 import sqlite3
-
+import logging
+import sys
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+# Database connections count
+db_connection_count = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    global db_connection_count
+    db_connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -36,13 +42,17 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info("Article %d not found" % post_id)
       return render_template('404.html'), 404
     else:
+      title = post['title']
+      app.logger.info("Article %s retrieved" % title)
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About Us page retrieved')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +70,34 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info("Article: %s Created" % title)
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+@app.route('/healthz')
+def healthz():
+    return {'result': 'OK - healthy'}, 200
+
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    post_count = len(posts)
+    data = {"db_connection_count": db_connection_count, "post_count": post_count}
+    return data
+
 # start the application on port 3111
 if __name__ == "__main__":
+   logging.basicConfig(filename='app.log',level=logging.DEBUG)
+   root = logging.getLogger()
+   root.setLevel(logging.DEBUG)
+   
+   handler = logging.StreamHandler(sys.stdout)
+   handler.setLevel(logging.DEBUG)
+   
+   formatter = logging.Formatter('%(asctime)s , %(message)s')
+   handler.setFormatter(formatter)
+   root.addHandler(handler)
    app.run(host='0.0.0.0', port='3111')
